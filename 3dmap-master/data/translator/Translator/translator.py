725f1afdc2d244095ab0ccb5f14bf266241c459f
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2016-11-14 08:46:20 +0100

    Fix case issues for running on case-sensitive OS

--- 3dmap-master/data/translator/Translator/translator.py
+++ 3dmap-master/data/translator/Translator/translator.py
@@ -18,10 +18,10 @@ class MapObject:
 
 
 class MapSquare:
-    
+
     def __init__(self, squareId, br):
         self.squareId = squareId
-        self.br = br        
+        self.br = br
 
 def createList(coordsString):
     coordList = []
@@ -35,9 +35,9 @@ def createList(coordsString):
 
 
 def getNorthWestCoordinate(coordList):
-    
+
     #for coord in coordList:
-        #print(coord)  
+        #print(coord)
     coordList.sort(key=lambda x: x[0])
     coordList.sort(key=lambda x: x[1], reverse=True)
     #print(coordList)
@@ -50,7 +50,7 @@ def getNorthWestCoordinate(coordList):
 def getSouthEastCoordinate(coordList):
     coordList.sort(key=lambda x: x[1])
     coordList.sort(key=lambda x: x[0], reverse=True)
-    
+
     return coordList[0]
 
 
@@ -61,7 +61,7 @@ def sortPlacemarks(placemarks):
     res = sorted(placemarks, key=lambda x: x.centroid[0])
     res = sorted(res,key=lambda x: centroid[1], reverse=True)
     anchor = res[0].centroid
-    
+
     res = sorted(res,key=lambda p: sqrt((p.centroid[0]-anchor[0])**2 + (p.centroid[1]-anchor[1])**2))
 
     return res
@@ -72,26 +72,26 @@ def createSquaresList(tl, br, divider):
         brcoordinates =  br.placemark.Polygon.outerBoundaryIs.LinearRing.coordinates
         brCoords = createList(brcoordinates)
         seCoord = getSouthEastCoordinate(brCoords)
-        
+
         print(nwCoord)
         print(seCoord)
         width = abs(nwCoord[0] - seCoord[0])
         height = abs(nwCoord[1]- seCoord[1])
-        
+
         length = max(width, height)
-        
+
         d = length/int(divider)
         c =  [i*d+d/2 for i in range(int(divider))]
-        
+
         squares = [(x,y) for x in c for y in c]
-        
+
         x = 0
         movedSquares = []
         for sq in squares:
             newPoint = (sq[0]+nwCoord[0], sq[1]+nwCoord[1])
             movedSquares.append(MapSquare(x,newPoint))
             x=x+1
-        
+
         return movedSquares
 
 def locate_centroid(coords):
@@ -102,12 +102,12 @@ def locate_centroid(coords):
 def get_grid_numbers(mapObjects,n):
     coords = np.array([o.coords for o in mapObjects][0])
     #coords = coords[0]
-    
+
     max_x, min_x = np.max(coords[:,0]), np.min(coords[:,0])
-    max_y, min_y = np.max(coords[:,1]), np.min(coords[:,1])    
+    max_y, min_y = np.max(coords[:,1]), np.min(coords[:,1])
     grid_size_x = (max_x - min_x) / float(n)
     grid_size_y = (max_y - min_y) / float(n)
-    
+
     print(str(max_x)+"  "+str(min_x))
     print(grid_size_y)
     for mapObject in mapObjects:
@@ -117,22 +117,22 @@ def get_grid_numbers(mapObjects,n):
         #make sure that for those coords which are the maximum lie in a valid grid number
         if y_ind == n : y_ind = n-1
         if x_ind == n : x_ind = n-1
-        
+
         grid_no = y_ind * n + x_ind + 1
-        
+
         mapObject.grid_no = grid_no
-    
+
     return mapObjects
 
 
-kml_file = path.join('../../', 'kml/8k/buildings.kml') 
+kml_file = path.join('../../', 'kml/8k/buildings.kml')
 name = None;
 with open(kml_file) as f:
 
     doc = parser.parse(f).getroot().Document.Folder
     name = doc.name
     placeMarks = []
-    
+
     for pm in doc.Placemark:
         coords = pm.Polygon.outerBoundaryIs.LinearRing.coordinates
         coords = createList(coords)
@@ -140,21 +140,21 @@ with open(kml_file) as f:
         centroid = [cx, cy]
 
         mapObject = MapObject(pm, centroid, coords)
-        
+
         placeMarks.append(mapObject)
 
-    
-       
+
+
     sortedPlacemarks = sortPlacemarks(placeMarks)
     griddedPlacemarks = get_grid_numbers(placeMarks, 3)
     #squareList = createSquaresList(sortedPlacemarks[0],sortedPlacemarks[len(sortedPlacemarks)-1],3)
-    
-        
+
+
     #sortedPlacemarks = sortedPlacemarks[:2000]
-    
+
     if name == None:
         name = KML.name("No Name")
-    
+
     doc = KML.Kml(
             KML.Document(
                 KML.Folder(
@@ -162,7 +162,7 @@ with open(kml_file) as f:
                            )
                         )
                   )
-    
+
     #for sor in sortedPlacemarks:
      #   squareId = 0
         #print(sor.nwCoord[1])
@@ -176,24 +176,19 @@ with open(kml_file) as f:
     for object in griddedPlacemarks:
         object.placemark.append(KML.description("geo-"+str(object.grid_no)))
         doc.Document.Folder.append(object.placemark)
-    
+
     #print(etree.tostring(doc, pretty_print=True))
-    
-      
+
+
     if placeMarks == sortedPlacemarks:
         print("equal")
     else:
         print("changed")
-        
-    savePath = path.join('../../', 'kml/8k/sortedBuildings.kml')
+
+    savePath = path.join('../../', 'kml/8k/sortedbuildings.kml')
     newKmlFile = open(savePath,'wb')
     new_kml = etree.tostring(doc, pretty_print=True)
     newKmlFile.write(new_kml)
     newKmlFile.close()
     print('done')
     #print(doc)
-
-
-
-
-        
